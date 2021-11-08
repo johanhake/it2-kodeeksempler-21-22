a563a8816184d2df6f3465846a25736512249932
Johan Hake <johan.hake@gmail.com>
2021-11-08 14:47:29 +0100

    La til mugshot mappe

---
+++ Kapittel 6-10/mugshots/elevnavn.js
@@ -0,0 +1,99 @@
+let elevNavn = [
+    ["Mia", "Austreim", "3STD"],
+    ["Philip", "Bennet", "3STF"],
+    ["Mats", "Ekeberg", "2STA"],
+    ["Roland", "Fiksdal", "3STC"],
+    ["Jonathan", "Gjefsen", "2STF"],
+    ["Kamilla", "Haukås", "2STD"],
+    ["Mathias", "Hestnes", "2STA"],
+    ["Erik", "Huse", "2STA"],
+    ["Erika", "Iden", "2STD"],
+    ["Hallvard", "Langen", "2STD"],
+    ["Isak", "Madland", "3STA"],
+    ["Haris", "Malik", "3STA"],
+    ["Nikolai", "Omvik", "2STD"],
+    ["Sindre", "Ravik", "3STB"],
+    ["Henrik", "Rædegård", "3STB"],
+    ["Mathias-s", "Schiøtz", "2STB"],
+    ["Nicolay", "Schiøtz", "2STB"],
+    ["Yaksh", "Shukla", "2STC"],
+    ["Andreas", "Ravik", "2STB"],
+    ["Jonas", "Sørli", "2STE"],
+    ["Thea", "Utgård", "3STC"],
+    ["Lukas", "Veidal", "2STB"],
+    ["Elias", "Westad", "2STF"],
+    ["Casper", "Yttervik", "2STE"],
+];
+
+let elevObjekt = [
+    {
+        "fornavn": "Eskild",
+        "etternavn": "Kamfjord",
+        "klasse": "3STE"
+    },
+    {
+        "fornavn": "Felix",
+        "etternavn": "Anthonisen",
+        "klasse": "3STA"
+    },
+    {
+        "fornavn": "Fredrik",
+        "etternavn": "Henriksen",
+        "klasse": "3STE"
+    },
+    {
+        "fornavn": "Helena",
+        "etternavn": "Larsen",
+        "klasse": "3STD"
+    },
+    {
+        "fornavn": "Ingvild",
+        "etternavn": "Sandven",
+        "klasse": "3STB"
+    },
+    {
+        "fornavn": "Magnus",
+        "etternavn": "Alstad",
+        "klasse": "3STE"
+    },
+    {
+        "fornavn": "Marius",
+        "etternavn": "Effersø",
+        "klasse": "3STC"
+    },
+    {
+        "fornavn": "Markus",
+        "etternavn": "Nord",
+        "klasse": "3STB"
+    },
+    {
+        "fornavn": "Michael",
+        "etternavn": "Eilertsen",
+        "klasse": "3STA"
+    },
+    {
+        "fornavn": "Oskar",
+        "etternavn": "Lothe",
+        "klasse": "3STE"
+    },
+    {
+        "fornavn": "Sam",
+        "etternavn": "Kanaan",
+        "klasse": "3STD"
+    },
+    {
+        "fornavn": "Sigurd",
+        "etternavn": "Dynna",
+        "klasse": "3STC"
+    },
+    {
+        "fornavn": "Sondre",
+        "etternavn": "Bærø",
+        "klasse": "3STA"
+    },
+    {
+        "fornavn": "Vetle",
+        "etternavn": "Svendsen",
+        "klasse": "3STC"
+    }
+];
